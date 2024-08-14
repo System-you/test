b0e33c40d310bf10e4703959cf31e98eaea99e7c
Jow12560 <jowrock555@gmail.com>
2024-08-14 21:56:24 +0700

    fix web

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
@@ -15,40 +15,32 @@ import Warehouse from "./views/pages/Warehouse";
 import WarehouseStock from "./views/pages/Warehouse/WarehouseStock";
 import TreasuryDocuments from "./views/pages/Warehouse/TreasuryDocuments";
 import NotFoundPage from "./views/pages/404";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const isLoggedIn = Boolean(localStorage.getItem('token'));
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    setIsLoggedIn(!!token);
-  }, []);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      
+      {/* Purchase */}
+      <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+      <Route path="/purchase" element={isLoggedIn ? <Purchase /> : <Navigate to="/login" />} />
+      <Route path="/deposit-document" element={isLoggedIn ? <DepositDocument /> : <Navigate to="/login" />} />
+      <Route path="/purchase-request" element={isLoggedIn ? <PurchaseRequest /> : <Navigate to="/login" />} />
+      <Route path="/purchase-order" element={isLoggedIn ? <PurchaseOrder /> : <Navigate to="/login" />} />
+      <Route path="/product-receipt" element={isLoggedIn ? <ProductReceipt /> : <Navigate to="/login" />} />
+      <Route path="/payment-voucher" element={isLoggedIn ? <PaymentVoucher /> : <Navigate to="/login" />} />
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Purchase */}
-        <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/purchase" element={isLoggedIn ? <Purchase /> : <Navigate to="/login" />} />
-        <Route path="/deposit-document" element={isLoggedIn ? <DepositDocument /> : <Navigate to="/login" />} />
-        <Route path="/purchase-request" element={isLoggedIn ? <PurchaseRequest /> : <Navigate to="/login" />} />
-        <Route path="/purchase-order" element={isLoggedIn ? <PurchaseOrder /> : <Navigate to="/login" />} />
-        <Route path="/product-receipt" element={isLoggedIn ? <ProductReceipt /> : <Navigate to="/login" />} />
-        <Route path="/payment-voucher" element={isLoggedIn ? <PaymentVoucher /> : <Navigate to="/login" />} />
+      {/* Warehouse */}
+      <Route path="/warehouse" element={isLoggedIn ? <Warehouse /> : <Navigate to="/login" />} />
+      <Route path="/warehouse-stock" element={isLoggedIn ? <WarehouseStock /> : <Navigate to="/login" />} />
+      <Route path="/treasury-documents" element={isLoggedIn ? <TreasuryDocuments /> : <Navigate to="/login" />} />
 
-        {/* Warehouse */}
-        <Route path="/warehouse" element={isLoggedIn ? <Warehouse /> : <Navigate to="/login" />} />
-        <Route path="/warehouse-stock" element={isLoggedIn ? <WarehouseStock /> : <Navigate to="/login" />} />
-        <Route path="/treasury-documents" element={isLoggedIn ? <TreasuryDocuments /> : <Navigate to="/login" />} />
+      {/* Login */}
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFoundPage />} />
 
-        {/* Login */}
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-root.render(<App />);
+    </Routes>
+  </BrowserRouter>
+);

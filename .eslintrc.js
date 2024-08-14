3145b17d2233686c62f70fe461a6ce260dccd789
Jow12560 <jowrock555@gmail.com>
2024-08-14 20:22:40 +0700

    Fixed ESLint issues and updated build configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+      'react-app',
+    ],
+    rules: {
+      'no-unused-vars': 'warn',  // Change this to 'off' if you want to ignore unused variables.
+      'jsx-a11y/anchor-is-valid': 'off',  // Disable the anchor-is-valid rule if it's too strict for your needs.
+    },
+  };
